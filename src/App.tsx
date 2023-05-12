b04ff6dd41d4c19bc3eedaa5fa6b9d9942d29322
iceniveth <ken11zer01@gmail.com>
2023-05-12 15:14:44 +0800

    Integrate React wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState, useRef } from "react";
-import TodoItem from "./TodoItem";
+import { Route, Switch } from "wouter";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 export type TodoItemType = {
   id: number;
@@ -8,59 +9,12 @@ export type TodoItemType = {
 };
 
 function App() {
-  const [task, setTask] = useState("");
-  const [todos, setTodos] = useState<TodoItemType[]>([]);
-  const taskRef = useRef<HTMLInputElement>(null);
-
-  const onTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTask(e.target.value);
-  };
-
-  const checkedTodosCount = todos.filter((t) => t.checked).length;
-
   return (
     <>
-      <input type="text" ref={taskRef} value={task} onChange={onTodoChange} />
-      <button
-        onClick={() => {
-          setTodos((todos) => [
-            ...todos,
-            { id: new Date().getTime(), task, checked: false },
-          ]);
-          setTask("");
-          taskRef.current?.focus();
-        }}
-      >
-        Add Todo
-      </button>
-      <button
-        onClick={() => {
-          setTodos([]);
-        }}
-      >
-        Clear Todos
-      </button>
-      <br />
-      <p>Checked Todos Count: {checkedTodosCount}</p>
-      <br />
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            onCheckedChanged={(id, checked) => {
-              setTodos((todos) =>
-                todos.map((t) =>
-                  t.id === id ? { ...t, checked } : t
-                )
-              );
-            }}
-            onDelete={(id) => {
-              setTodos((todos) => todos.filter((t) => t.id !== id));
-            }}
-          />
-        ))}
-      </ul>
+      <Switch>
+        <Route path="/products" component={Products} />
+        <Route path="/" component={Home} />
+      </Switch>
     </>
   );
 }
