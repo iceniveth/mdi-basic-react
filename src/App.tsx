b1ed1c89d283904e3245e12da7708862068909fd
iceniveth <ken11zer01@gmail.com>
2023-05-13 10:35:43 +0800

    Add product pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Route, Switch } from "wouter";
+import { Link, Route, Switch } from "wouter";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import ViewProduct from "./pages/ViewProduct";
 
 export type TodoItemType = {
   id: number;
@@ -11,7 +12,13 @@ export type TodoItemType = {
 function App() {
   return (
     <>
+      <div style={{ display: "flex", gap: "8px" }}>
+        <Link href="/">Home</Link>
+        <Link href="/products">Products</Link>
+      </div>
+
       <Switch>
+        <Route path="/products/:productId" component={ViewProduct} />
         <Route path="/products" component={Products} />
         <Route path="/" component={Home} />
       </Switch>
---
+++ src/pages/ViewProduct.tsx
@@ -0,0 +1,9 @@
+export default function ViewProduct() {
+  return (
+    <>
+      <h2>View Product</h2>
+      {/* TODO implement viewing of product page. 
+      The API of "Get a single product": https://fakestoreapi.com/docs */}
+    </>
+  );
+}
