714f34af7c615cfabb9d8bdff9e628a9324afd5c
iceniveth <ken11zer01@gmail.com>
2023-05-13 10:47:15 +0800

    Transfer TODO app under '/' route

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,59 @@
+import { useRef, useState } from "react";
+import { TodoItemType } from "../App";
+import TodoItem from "../TodoItem";
+
 export default function Home() {
-  return <h2>Home</h2>;
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState<TodoItemType[]>([]);
+  const taskRef = useRef<HTMLInputElement>(null);
+
+  const onTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value);
+  };
+
+  const checkedTodosCount = todos.filter((t) => t.checked).length;
+
+  return (
+    <>
+      <input type="text" ref={taskRef} value={task} onChange={onTodoChange} />
+      <button
+        onClick={() => {
+          setTodos((todos) => [
+            ...todos,
+            { id: new Date().getTime(), task, checked: false },
+          ]);
+          setTask("");
+          taskRef.current?.focus();
+        }}
+      >
+        Add Todo
+      </button>
+      <button
+        onClick={() => {
+          setTodos([]);
+        }}
+      >
+        Clear Todos
+      </button>
+      <br />
+      <p>Checked Todos Count: {checkedTodosCount}</p>
+      <br />
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onCheckedChanged={(id, checked) => {
+              setTodos((todos) =>
+                todos.map((t) => (t.id === id ? { ...t, checked } : t))
+              );
+            }}
+            onDelete={(id) => {
+              setTodos((todos) => todos.filter((t) => t.id !== id));
+            }}
+          />
+        ))}
+      </ul>
+    </>
+  );
 }
