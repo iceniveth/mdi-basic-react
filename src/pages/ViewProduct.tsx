b60e70d5170bc6d9e2f6d9c137423b4595f9c2c5
iceniveth <ken11zer01@gmail.com>
2023-05-15 11:35:17 +0800

    Fetch product from fakestoreapi

--- src/pages/ViewProduct.tsx
+++ src/pages/ViewProduct.tsx
@@ -1,4 +1,18 @@
-export default function ViewProduct() {
+import { FunctionComponent, useEffect, useState } from "react";
+import { RouteComponentProps } from "wouter";
+import { Product } from "./Products";
+
+const ViewProduct: FunctionComponent<RouteComponentProps> = ({ params }) => {
+  const [product, setProduct] = useState<Product | undefined>();
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${params.productId}`)
+      .then((res) => res.json())
+      .then((json) => setProduct(json));
+  }, []);
+
+  console.log(product)
+
   return (
     <>
       <h2>View Product</h2>
@@ -6,4 +20,6 @@ export default function ViewProduct() {
       The API of "Get a single product": https://fakestoreapi.com/docs */}
     </>
   );
-}
+};
+
+export default ViewProduct;
